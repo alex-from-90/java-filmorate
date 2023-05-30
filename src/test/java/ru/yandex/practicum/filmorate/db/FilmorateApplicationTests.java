583ea225a02e55479abc6ef96828bf9ea101dd9e
alex-from-90 <alex@site-n.ru>
2023-05-31 01:22:31 +0300

    CodeStyle 4

--- src/test/java/ru/yandex/practicum/filmorate/db/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/db/FilmorateApplicationTests.java
@@ -177,9 +177,7 @@ class FilmorateApplicationTests {
     @Test
     public void testDeleteFilm() {
         Film createdFirstFilm = filmStorage.create(firstFilm);
-
         filmStorage.delete(createdFirstFilm.getId());
-
         List<Film> listFilms = filmStorage.getFilms();
         assertThat(listFilms).hasSize(1);
         assertThat(listFilms.get(0))
