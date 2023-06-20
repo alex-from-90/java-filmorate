49952e4f60dca725ac507f70601ae151c5906f8e
nik110996 <nikitos110996@yandex.ru>
2023-06-20 14:30:24 +0400

    19.06.2023_Second commit3

--- src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/UserStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.interfaces;
 
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
