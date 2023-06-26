f18671f048968d993cf20a4dacb44e1c0b06527e
Evgenii <118066296+DayZscripter@users.noreply.github.com>
2023-06-26 17:16:33 +0300

    Update FilmService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -57,4 +57,8 @@ public class FilmService {
     public List<Film> getCommonFilms(long userId, long friendId) {
         return likeStorage.getCommonFilms(userId, friendId);
     }
+
+        public List<Film> filmSearch(String query, String by) {
+        return filmStorage.filmsSearch(query, by);
+    }
 }
